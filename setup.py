bedddef569313284fdb1920cffe04452bd7a927a
Alex Liberzon <alex.liberzon@gmail.com>
2017-10-05 22:22:27 +0300

    copied setup.py from simplejson - so the main idea is to create C code in the same directory and just use put everything in __init__.py with some import and return of a function

--- setup.py
+++ setup.py
@@ -1,81 +1,126 @@
-import logging
-import numpy
+#!/usr/bin/env python
+from __future__ import with_statement
 
+import sys
 try:
-    from setuptools import setup
-    from setuptools import Extension
-    from setuptools.command.build_ext import build_ext
-    # from Cython.Distutils import build_ext
+    from setuptools import setup, Extension, Command
 except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
-    from distutils.command.build_ext import build_ext
+    from distutils.core import setup, Extension, Command
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, \
+    DistutilsPlatformError
 
-# from setuptools.extension import Extension
-from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+VERSION = '3.7.3'
+DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
-logging.basicConfig()
-log = logging.getLogger(__file__)
+with open('README.rst', 'r') as f:
+   LONG_DESCRIPTION = f.read()
 
-ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError, IOError)
+CLASSIFIERS = filter(None, map(str.strip,
+"""
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+License :: OSI Approved :: Academic Free License (AFL)
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.5
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: Implementation :: CPython
+Programming Language :: Python :: Implementation :: PyPy
+Topic :: Software Development :: Libraries :: Python Modules
+""".splitlines()))
+
+if sys.platform == 'win32' and sys.version_info > (2, 6):
+   # 2.6's distutils.msvc9compiler can raise an IOError when failing to
+   # find the compiler
+   # It can also raise ValueError http://bugs.python.org/issue7511
+   ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+                 IOError, ValueError)
+else:
+   ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
 
 class BuildFailed(Exception):
     pass
 
-def construct_build_ext(build_ext):
-    class WrappedBuildExt(build_ext):
-        # This class allows C extension building to fail.
-        def run(self):
-            try:
-                build_ext.run(self)
-            except DistutilsPlatformError as x:
-                raise BuildFailed(x)
+class ve_build_ext(build_ext):
+    # This class allows C extension building to fail.
 
-        def build_extension(self, ext):
-            try:
-                build_ext.build_extension(self, ext)
-            except ext_errors as x:
-                raise BuildFailed(x)
-    return WrappedBuildExt
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            raise BuildFailed()
 
-setup_args = {'name': 'openpiv', 'license': 'GPL', 'author': 'OpenPIV',
-    'packages': ['openpiv', 'openpiv.py_src', 'openpiv.c_src'],
-    'cmdclass': {}
-    }
-    
-USE_CYTHON = False  # command line option, try-import, ...
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            raise BuildFailed()
 
-ext = '.pyx' if USE_CYTHON else '.c'
 
-ext_modules = [Extension(
-                        name         = "openpiv.process",
-                        sources      = ["openpiv/c_src/process"+ext],
-                        include_dirs = [numpy.get_include()],
-                        ),
-                Extension(    name         = "openpiv.lib",
-                        sources      = ["openpiv/c_src/lib"+ext],
-                        include_dirs = [numpy.get_include()],
-                    )
-                ]
+class TestCommand(Command):
+    user_options = []
 
-cmd_classes = setup_args.setdefault('cmdclass', {})
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+        raise SystemExit(
+            subprocess.call([sys.executable,
+                             # Turn on deprecation warnings
+                             '-Wd',
+                             'simplejson/tests/__init__.py']))
+
+def run_setup(with_binary):
+    cmdclass = dict(test=TestCommand)
+    if with_binary:
+        kw = dict(
+            ext_modules = [
+                Extension("simplejson._speedups", ["simplejson/_speedups.c"]),
+            ],
+            cmdclass=dict(cmdclass, build_ext=ve_build_ext),
+        )
+    else:
+        kw = dict(cmdclass=cmdclass)
+
+    setup(
+        name="simplejson",
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        classifiers=CLASSIFIERS,
+        author="Bob Ippolito",
+        author_email="bob@redivi.com",
+        url="http://github.com/simplejson/simplejson",
+        license="MIT License",
+        packages=['simplejson', 'simplejson.tests'],
+        platforms=['any'],
+        **kw)
 
 try:
-    # try building with c code :
-    setup_args['cmdclass']['build_ext'] = construct_build_ext(build_ext)
-    setup(ext_modules=ext_modules, **setup_args)
-except BuildFailed as ex:
-    log.warn(ex)
-    log.warn("The C extension could not be compiled")
+    run_setup(not IS_PYPY)
+except BuildFailed:
+    BUILD_EXT_WARNING = ("WARNING: The C extension could not be compiled, "
+                         "speedups are not enabled.")
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Failure information, if any, is above.")
+    print("I'm retrying the build without the C extension now.")
+    print('*' * 75)
 
-    ## Retry to install the openpiv without C extensions :
-    # Remove any previously defined build_ext command class.
-    if 'build_ext' in setup_args['cmdclass']:
-        del setup_args['cmdclass']['build_ext']
-    if 'build_ext' in cmd_classes:
-        del cmd_classes['build_ext']
+    run_setup(False)
 
-    # If this new 'setup' call don't fail, the openpiv 
-    # will be successfully installed, without the C extension :
-    setup(**setup_args)
-    log.info("Plain-Python installation succeeded.")
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Plain-Python installation succeeded.")
+    print('*' * 75)
